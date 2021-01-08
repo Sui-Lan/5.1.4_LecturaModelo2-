0f3927d3d86e252225c22ee886c11dd5bd62196b
Sui <sgonzalezmac@gmail.com>
2021-01-07 22:21:48 -0300

    AppConfig

---
+++ 5.1.4_LecturaModelo2/src/main/java/com/repaso/AppConfig.java
@@ -0,0 +1,33 @@
+package com.repaso;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.repaso")
+@PropertySource("classpath:database.properties")
+public class AppConfig {
+
+    @Autowired
+    Environment environment;
+    
+    @Bean
+    DataSource dataSource() {
+	DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+	driverManagerDataSource.setDriverClassName(environment.getProperty("driver"));
+	driverManagerDataSource.setUrl(environment.getProperty("url"));
+	driverManagerDataSource.setUsername(environment.getProperty("usuario"));
+	driverManagerDataSource.setPassword(environment.getProperty("clave"));
+	
+	return driverManagerDataSource;
+    }
+    
+}
